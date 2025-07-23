1257e2836fdf6c3108c2a18db7c647f051754b75
fitriyanianisa18 <fitriyanianisa18@gmail.com>
2025-07-23 20:54:03 +0700

    Menambahkan state reducer

--- src/app/todo-list/page.jsx
+++ src/app/todo-list/page.jsx
@@ -5,16 +5,6 @@ import TodoInput from '@/components/todoComponents/TodoInput'
 import TodoList from '@/components/todoComponents/todoList'
 
 export default function Home() {
-  // const hasTodos = todos.length > 0
-  // const isLanguageIndonesian = false
-
-  // let list 
-  //   if (!hasTodos) {
-  //   list = <p className='text-center text-gray-500'>Task is empty. Please create a task.</p>;
-  //   } else {
-  //   list = <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo}/>;
-  //   }
-
   return (
     <TodoProvider>
             <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
@@ -22,33 +12,6 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-center">My Todo List</h1>
         <TodoInput/>
         <TodoList/>
-
-        {/* 1. if-else */}
-          {/* {(() => {
-            if (!hasTodos) {
-              return <p className='text-center text-gray-500'>Task is empty. Please create a task.</p>
-            } else {
-              return <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo}/>;
-            }
-          })()} */}
-          
-          {/* 2. Ternary Operator */}
-          {/* 2 kondisi */}
-          {/* {hasTodos ? <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo}/> : <p className='text-center text-gray-500'>Task is empty. Please create a task.</p>}       */}
-          {/* 3 kondisi */}
-          {/* {hasTodos ? (
-          <TodoList/>
-            ) : isLanguageIndonesian ? (
-          <p className="text-center text-gray-500">Task kosong, silahkan buat Task baru.</p>
-            ) : (
-          <p className="text-center text-gray-500">Task is empty. Please create a task</p>
-          )} */}
-
-          {/* 3. Logical AND (&&) */}
-          {/* {hasTodos && <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo}/>}
-          {!hasTodos && <p className='text-center text-gray-500'>Task is empty. Please create a task.</p>} */}
-
-          {/* {list} */}
       </div>
     </main>
     </TodoProvider>
--- src/components/todoComponents/TodoInput.jsx
+++ src/components/todoComponents/TodoInput.jsx
@@ -3,23 +3,23 @@
 import {useTodos} from '@/context/todoContext'
 
 export default function TodoInput() {
-  const {input, setInput, addTodo} = useTodos()
+  const {state, dispatch} = useTodos()
 
   const handleChange = (e) => {
     console.log('Change', e.target.value)
-    setInput(e.target.value)
+    dispatch({type: "SET_INPUT", payload: e.target.value})
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log("Input is submitted")
-    addTodo()
+    dispatch({type: "ADD_TODO"})
   }
   return (
     <form onSubmit={handleSubmit} className="flex gap-2">
       <input
         type="text"
-        value={input}
+        value={state.input}
         onChange={handleChange}
         placeholder="Masukkan Task Baru"
         className="flex-grow px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
